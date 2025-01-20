9cec6cc1b291ae36b75007b2ad5ed39159954df9
Abdulwahab Isam <abdoi94.iq@gmail.com>
2025-01-20 19:12:13 +0300

    pipa: Blissify
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

---
+++ bliss_pipa.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit some common bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
+
+# Inherit from pipa device
+$(call inherit-product, device/xiaomi/pipa/device.mk)
+
+# Inherit keys
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+
+PRODUCT_NAME := bliss_pipa
+PRODUCT_DEVICE := pipa
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Pad 6
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Xiaomi/pipa_global/pipa:13/RKQ1.211001.001/V816.0.7.0.UMZMIXM:user/release-keys
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
