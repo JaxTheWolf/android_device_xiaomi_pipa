80649860f7755706ed1d0f7bc12740f5901d4731
Roman Lubij <roman.lubij@gmail.com>
2025-04-30 17:52:35 +0200

    pipa: Include updater

--- device.mk
+++ device.mk
@@ -42,6 +42,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     XiaomiPeripheralManager
 
+# Updater
+PRODUCT_PACKAGES += \
+	Updater
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
